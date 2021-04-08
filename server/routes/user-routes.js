1a7d58c51c79d84f4f17f024635010a7f1323644
Kay0s <hamilton.kristina@gmail.com>
2021-04-08 16:29:04 -0500

    added image to the database response

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -33,12 +33,13 @@ router.get('/users/:username', (req, res) => {
     ExpressionAttributeNames: {
       "#un": "username",
       "#ca": "createdAt",
-      "#th": "thought"
+      "#th": "thought",
+      "#img": "image"  // add the imiage attribute alias
     },
     ExpressionAttributeValues: {
       ":user": req.params.username
     },
-    ProjectionExpression: "#th, #ca",
+    ProjectionExpression: "#un, #th, #ca, #img",  // add the image to the database response
     ScanIndexForward: false
   };
 
