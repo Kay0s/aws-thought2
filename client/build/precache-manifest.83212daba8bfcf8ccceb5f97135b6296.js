10dd5f8524afbcd405b919e49c2a436028b860ca
Kay0s <hamilton.kristina@gmail.com>
2021-04-10 17:58:39 -0500

    removed build from ignore

---
+++ client/build/precache-manifest.83212daba8bfcf8ccceb5f97135b6296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71077692532eff1bdb152b3bc6624638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4e9745fad6e954ae6f4",
+    "url": "/static/css/main.4a34557b.chunk.css"
+  },
+  {
+    "revision": "f43a08d880dc6e073bee",
+    "url": "/static/js/2.933cacc0.chunk.js"
+  },
+  {
+    "revision": "0ec9300b90a1964ccb21b13048e7fb5c",
+    "url": "/static/js/2.933cacc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e9745fad6e954ae6f4",
+    "url": "/static/js/main.ff2ceee3.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  }
+]);
